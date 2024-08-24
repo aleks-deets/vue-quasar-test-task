d6322d962749516af5a3c1b599dfc2232f51e9ca
aleks-deets <oneofmycontacts5@gmail.com>
2024-08-24 16:48:18 +0300

    Changes Docker files. Created ASP.NET backend application.

---
+++ backend-aspnet/backend-aspnet/Pages/Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_aspnet.Pages.Controllers
+{
+    [Route("api/items")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = new[]
+            {
+                new { Item = "First" },
+                new { Item = "Second" }
+            };
+            return Ok(items);
+        }
+    }
+}
